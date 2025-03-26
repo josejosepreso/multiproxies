ad2718269bc52f1db45a9aee01b943139c4ceca7
josejosepreso <josehihat@gmail.com>
2025-03-26 16:24:46 -0600

    tbody

--- app/Main.hs
+++ app/Main.hs
@@ -3,23 +3,28 @@ import Text.Regex.Posix
 import Text.HTML.TagSoup
 import Data.Foldable (fold)
 
-parsetbl :: [Tag String] -> [String]
-parsetbl [] = []
-parsetbl (x:xs)
-  | notipport x = parsetbl xs
-  | otherwise = [fromTagText x] ++ (parsetbl xs)
-  where notipport :: Tag String -> Bool
-        notipport tag = not . and $ [ isTagText tag,
-                                  or [ (fromTagText tag =~ "^[0-9\\.]+$") :: Bool,
-                                       (fromTagText tag =~ "^[0-9]+$") :: Bool
-                                     ]
-                                ]
+type Proxy = (String, String)
+
+parseTbl :: [Tag String] -> [Proxy]
+parseTbl [] = []
+parseTbl (x:xs)
+  | ip x = [( fromTagText x
+            , fromTagText . head . dropWhile (not . port) $ xs
+            )] ++ parseTbl xs
+  | otherwise = parseTbl xs
+  where ip, port :: Tag String -> Bool
+        ip tag = and [ isTagText tag
+                     , fromTagText tag =~ "^[0-9]+\\.[0-9]+\\.[0-9]+\\.[0-9]+$" :: Bool
+                     ]
+        port tag = and [ isTagText tag
+                       , fromTagText tag =~ "^[0-9]+$" :: Bool
+                       ]
 
 sources :: [IO String]
 sources = [ curlGetString "https://spys.me/proxy.txt" [CurlNoProgress True]
             >>= pure
             . unlines
-            . map getpair
+            . map matchPair
             . init . init . drop 6
             . lines
             . snd
@@ -27,17 +32,17 @@ sources = [ curlGetString "https://spys.me/proxy.txt" [CurlNoProgress True]
             curlGetString "https://free-proxy-list.net/" [CurlNoProgress True]
             >>= pure
             . unlines
-            . topair
-            . parsetbl
+            . toPair
+            . parseTbl
             . parseTags
             . tbody
             . snd
           ]
-  where getpair :: String -> String
-        getpair x = x =~ "[0-9\\.]+:[0-9]+" :: String
-        tbody :: String -> String
+  where tbody :: String -> String
         tbody x = x =~ "<tbody>.*</tbody>" :: String
-        topair [] = []
-        topair (x:y:xs) = [x ++ ":" ++ y] ++ topair xs
+        matchPair :: String -> String
+        matchPair x = x =~ "[0-9\\.]+:[0-9]+" :: String
+        toPair [] = []
+        toPair ((ip, port):xs) = [ip ++ ":" ++ port] ++ toPair xs
 
 main = fold sources >>= writeFile "proxies.txt"
